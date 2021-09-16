591f618dfaa57d48eac615f23bbccc653c28aa47
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-09-16 14:06:24 +0300

    add views, components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Page404 from './Views/Page404';
 
 function App() {
   return (
     <div>
-      
-    </div>
-  );
+    <Router>
+        <Header />
+        <Switch>
+
+            <Route exact path="/">
+                <Home />
+            </Route>
+
+            <Route path="/articles">
+                <Articles />
+            </Route>
+
+            <Route path="/article">
+                <Article />
+            </Route>
+
+            <Route path="/chat">
+                <Chat/>  
+            </Route>
+
+            <Route path="/register">
+                <Register />
+            </Route>
+
+            <Route path="/pageaboutus">
+                <PageAboutUs />
+            </Route>
+
+            <Route>
+                <Page404 />
+            </Route>
+
+        </Switch>
+        <Footer />
+    </Router>
+
+</div>
+);
 }
+
 
 export default App;
