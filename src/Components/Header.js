5a79755cf56d3d06bc0aaed2d7f4ac6d5b35d683
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-09-24 23:11:54 +0300

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,14 +3,14 @@ function Header() {
     return (
         <div>
 
-            <header class="py-3 mb-4 border-bottom">
-                <div class="container d-flex flex-wrap justify-content-center">
-                    <a href="/" class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">
-                        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap" /></svg>
-                        <span class="fs-4">Double header</span>
+            <header className="py-3 mb-4 border-bottom">
+                <div className="container d-flex flex-wrap justify-content-center">
+                    <a href="/" className="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">
+                        <svg className="bi me-2"style={{width: '40', height: '32'}}><use href="#bootstrap" /></svg>
+                        <span className="fs-4">Double header</span>
                     </a>
-                    <form class="col-12 col-lg-auto mb-3 mb-lg-0">
-                        <input type="search" class="form-control" placeholder="Search..." aria-label="Search"/>
+                    <form className="col-12 col-lg-auto mb-3 mb-lg-0">
+                        <input type="search" className="form-control" placeholder="Search..." aria-label="Search"/>
                     </form>
                 </div>
             </header>
