0b585ef50d6a6ac5dec9ad0878522f3a44896550
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-09-25 20:38:29 +0300

    Update Page404.js

--- src/Views/Page404.js
+++ src/Views/Page404.js
@@ -5,10 +5,6 @@ function Page404() {
             <div id="world"></div>
             <div id="instructions">Press and drag to make wind<br /><span className="lightInstructions">the lion will surely appreciate</span></div>
 
-            <div id="credits">
-                <p>Prints on <a className="society6" href="https://society6.com/yakudoo/" target="blank">society6</a> | <a href="https://codepen.io/Yakudoo/" target="blank">my other codepens</a> | <a href="https://www.epic.net" target="blank">epic.net</a></p>
-            </div>
-
 
         </div>
     )
