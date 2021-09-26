6a3489114d87d83b725556f4b87a5f9b0b5076c8
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-09-26 20:55:17 +0300

    cleaning code

--- src/Views/Page404.js
+++ src/Views/Page404.js
@@ -5,15 +5,7 @@ function Page404() {
 
     return (
         <div>
-            <div className="cont_principal">
-                <div className="cont_error">
-
-                    <h1>Oops</h1>
-                    <p>The Page you're looking for isn't here.</p>
-                </div>
-                <div className="cont_aura_1"></div>
-                <div className="cont_aura_2"></div>
-            </div>
+            
 
         </div>
     )
