895062d1381a9659b00cab7e2a26baebffd8ab5a
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-09-27 16:23:04 +0300

    Update Page404.js

--- src/Views/Page404.js
+++ src/Views/Page404.js
@@ -5,18 +5,18 @@ function Page404() {
 
     return (
         <div>
-            <div class="mars"></div>
-            <img src="https://assets.codepen.io/1538474/404.svg" class="logo-404" />
-            <img src="https://assets.codepen.io/1538474/meteor.svg" class="meteor" />
-            <p class="title">Oh no!!</p>
-            <p class="subtitle">
+            <div className="mars"></div>
+            <img src="https://assets.codepen.io/1538474/404.svg" className="logo-404" />
+            <img src="https://assets.codepen.io/1538474/meteor.svg" className="meteor" />
+            <p className="title">Oh no!!</p>
+            <p className="subtitle">
                 You’re either misspelling the URL <br /> or requesting a page that's no longer here.
             </p>
             <div align="center">
-                <a class="btn-back" href="#">Back to previous page</a>
+                <a className="btn-back" href="#">Back to previous page</a>
             </div>
-            <img src="https://assets.codepen.io/1538474/astronaut.svg" class="astronaut" />
-            <img src="https://assets.codepen.io/1538474/spaceship.svg" class="spaceship" />
+            <img src="https://assets.codepen.io/1538474/astronaut.svg" className="astronaut" />
+            <img src="https://assets.codepen.io/1538474/spaceship.svg" className="spaceship" />
         </div>
     )
 }
