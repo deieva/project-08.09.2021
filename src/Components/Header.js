5b5ab982c13e11419e379463404f512e4754f71a
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-10-01 12:54:47 +0300

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,10 +7,8 @@ function Header() {
                 <div className="container d-flex flex-wrap">
                     <ul className="nav me-auto">
                         <li className="nav-item"><a href="#" className="nav-link link-dark px-2 active" aria-current="page">Home</a></li>
-                        <li className="nav-item"><a href="#" className="nav-link link-dark px-2">Features</a></li>
-                        <li className="nav-item"><a href="#" className="nav-link link-dark px-2">Pricing</a></li>
-                        <li className="nav-item"><a href="#" className="nav-link link-dark px-2">FAQs</a></li>
-                        <li className="nav-item"><a href="#" className="nav-link link-dark px-2">About</a></li>
+                        <li className="nav-item"><a href="#" className="nav-link link-dark px-2">Categories</a></li>
+                        <li className="nav-item"><a href="#" className="nav-link link-dark px-2">Cart</a></li>
                     </ul>
                     <ul className="nav">
                         <li className="nav-item"><a href="#" className="nav-link link-dark px-2">Login</a></li>
