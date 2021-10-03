73d740bcbe5922ff6a04daf65de4147a76eb4b24
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-10-03 14:09:14 +0300

    style for login

--- src/Views/Login.js
+++ src/Views/Login.js
@@ -5,7 +5,7 @@ function Login() {
     return (
         <div>
 
-<div className="container">
+<div className="login-container">
 	<div className="d-flex justify-content-center h-100">
 		<div className="card">
 			<div className="card-header">
