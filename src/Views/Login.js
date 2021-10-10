6b747ecc213846d23de5f312a147fcc7c8662a13
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-10-10 15:14:02 +0300

    Update Login.js

--- src/Views/Login.js
+++ src/Views/Login.js
@@ -1,5 +1,4 @@
-// import { faFortAwesome } from '@fortawesome/free-brands-svg-icons';
-// import { faSquare } from '@fortawesome/free-regular-svg-icons';
+
 import '../Assets/Css/Login.css';
 
 function Login() {
