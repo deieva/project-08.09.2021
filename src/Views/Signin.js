9a10651913b2a921e500282c21ce51b7eebf4539
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-10-11 09:41:02 +0300

    add signin, i jquery

--- src/Views/Signin.js
+++ src/Views/Signin.js
@@ -1,9 +1,100 @@
-
+import '../Assets/Css/Signin.css';
+import $ from 'jquery';
 
 function Signin() {
 
+    let i=0;
+	$(document).ready(function(){
+     $('#add_more').on('click', function(){
+      let colorR = Math.floor((Math.random() * 256));
+      let colorG = Math.floor((Math.random() * 256));
+      let colorB = Math.floor((Math.random() * 256));
+      i++;
+      let html ='<div id="append_no_'+i+'" class="animated bounceInLeft">'+
+          '<div class="input-group mt-3">'+
+		  '<div class="input-group-prepend">'+
+		  '<span class="input-group-text br-15" style="color:rgb('+colorR+','+colorG+','+colorB+'">'+
+		  '<i class="fas fa-user-graduate"></i></span>'+
+		  '</div>'+
+		  '<input type="text" placeholder="Student Name"  class="form-control"/>'+
+		  '</div>'+
+		  '<div class="input-group mt-3">'+
+		  '<div class="input-group-prepend">'+
+		  '<span class="input-group-text br-15" style="color:rgb('+colorR+','+colorG+','+colorB+'">'+
+		  '<i class="fas fa-phone-square"></i></span>'+
+		  '</div>'+
+		  '<input type="text" placeholder="Student Phone" class="form-control"/>'+
+		  '</div>'+
+		  '<div class="input-group mt-3">'+
+		  '<div class="input-group-prepend">'+
+		  '<span class="input-group-text br-15" style="color:rgb('+colorR+','+colorG+','+colorB+'">'+
+		  '<i class="fas fa-at"></i></span>'+
+		  '</div>'+
+		  '<input type="email" placeholder="Student Email" class="form-control"/>'+
+		  '</div></div>';
+
+	  $('#dynamic_container').append(html);
+	  $('#remove_more').fadeIn(function(){
+	  	 $(this).show();
+	  });
+     });
+
+     $('#remove_more').on('click', function(){
+         
+         $('#append_no_'+i).removeClass('bounceInLeft').addClass('bounceOutRight')
+            .fadeOut(function(){
+            	$(this).remove();
+            });
+            i--;
+            if(i==0){
+            	$('#remove_more').fadeOut(function(){
+            		$(this).hide()
+            	});;
+            }
+   
+     });
+	});
+
     return (
         <div>
+            	<dvi class="container h-100">
+	<div class="d-flex justify-content-center">
+		<div class="card mt-5 col-md-4 animated bounceInDown myForm">
+			<div class="card-header">
+				<h4>Students Contact Details</h4>
+			</div>
+			<div class="card-body">
+				<form>
+					<div id="dynamic_container">
+						<div class="input-group">
+							<div class="input-group-prepend">
+								<span class="input-group-text br-15"><i class="fas fa-user-graduate"></i></span>
+							</div>
+							<input type="text" placeholder="Student Name" class="form-control"/>
+						</div>
+						<div class="input-group mt-3">
+							<div class="input-group-prepend">
+								<span class="input-group-text br-15"><i class="fas fa-phone-square"></i></span>
+							</div>
+							<input type="text" placeholder="Student Phone" class="form-control"/>
+						</div>
+						<div class="input-group mt-3">
+							<div class="input-group-prepend">
+								<span class="input-group-text br-15"><i class="fas fa-at"></i></span>
+							</div>
+							<input type="email" placeholder="Student Email" class="form-control"/>
+						</div>
+					</div>
+				</form>
+			</div>
+			<div class="card-footer">
+				<a class="btn btn-secondary btn-sm" id="add_more"><i class="fas fa-plus-circle"></i> Add</a>
+				<a class="btn btn-secondary btn-sm" id="remove_more"><i class="fas fa-trash-alt"></i> Remove</a>
+				<button class="btn btn-success btn-sm float-right submit_btn"><i class="fas fa-arrow-alt-circle-right"></i> Submit</button>
+			</div>
+		</div>
+	</div>
+	</dvi>
 
         </div>
     )
