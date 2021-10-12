993f086650eb69bda710d3e0e02f1ec12dec7e2a
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-10-12 09:40:57 +0300

    Update Signin.js

--- src/Views/Signin.js
+++ src/Views/Signin.js
@@ -61,7 +61,7 @@ function Signin() {
 	<div class="d-flex justify-content-center">
 		<div class="card mt-5 col-md-4 animated bounceInDown myForm">
 			<div class="card-header">
-				<h4>Students Contact Details</h4>
+				<h4>Contact Details</h4>
 			</div>
 			<div class="card-body">
 				<form>
@@ -70,25 +70,25 @@ function Signin() {
 							<div class="input-group-prepend">
 								<span class="input-group-text br-15"><i class="fas fa-user-graduate"></i></span>
 							</div>
-							<input type="text" placeholder="Student Name" class="form-control"/>
+							<input type="text" placeholder="Name" class="form-control"/>
 						</div>
 						<div class="input-group mt-3">
 							<div class="input-group-prepend">
 								<span class="input-group-text br-15"><i class="fas fa-phone-square"></i></span>
 							</div>
-							<input type="text" placeholder="Student Phone" class="form-control"/>
+							<input type="text" placeholder="Phone" class="form-control"/>
 						</div>
 						<div class="input-group mt-3">
 							<div class="input-group-prepend">
 								<span class="input-group-text br-15"><i class="fas fa-at"></i></span>
 							</div>
-							<input type="email" placeholder="Student Email" class="form-control"/>
+							<input type="email" placeholder="Email" class="form-control"/>
 						</div>
 					</div>
 				</form>
 			</div>
 			<div class="card-footer">
-				<a class="btn btn-secondary btn-sm" id="add_more"><i class="fas fa-plus-circle"></i> Add</a>
+				{/* <a class="btn btn-secondary btn-sm" id="add_more"><i class="fas fa-plus-circle"></i> Add</a> */}
 				<a class="btn btn-secondary btn-sm" id="remove_more"><i class="fas fa-trash-alt"></i> Remove</a>
 				<button class="btn btn-success btn-sm float-right submit_btn"><i class="fas fa-arrow-alt-circle-right"></i> Submit</button>
 			</div>
