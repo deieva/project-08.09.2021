17c151996b87352b32549dba343788aa0f96d53d
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-10-24 12:38:26 +0300

    add tabs to categories

--- src/Views/Categories.js
+++ src/Views/Categories.js
@@ -1,38 +1,26 @@
+import '../Assets/Css/Categories.css';
+
 function Categories() {
 
     return (
 
         <div>
 
-            <div className="container"><h1>Bootstrap  tab panel example (using nav-pills)  </h1></div>
-            <div id="exTab1" className="container">
-                <ul className="nav nav-pills">
-                    <li className="active">
-                        <a href="#1a" data-toggle="tab">Overview</a>
-                    </li>
-                    <li><a href="#2a" data-toggle="tab">Using nav-pills</a>
-                    </li>
-                    <li><a href="#3a" data-toggle="tab">Applying clearfix</a>
-                    </li>
-                    <li><a href="#4a" data-toggle="tab">Background color</a>
-                    </li>
-                </ul>
-
-                <div className="tab-content clearfix">
-                    <div className="tab-pane active" id="1a">
-                        <h3>Content's background color is the same for the tab</h3>
-                    </div>
-                    <div className="tab-pane" id="2a">
-                        <h3>We use the className nav-pills instead of nav-tabs which automatically creates a background color for the tab</h3>
-                    </div>
-                    <div className="tab-pane" id="3a">
-                        <h3>We applied clearfix to the tab-content to rid of the gap between the tab and the content</h3>
-                    </div>
-                    <div className="tab-pane" id="4a">
-                        <h3>We use css to change the background color of the content to be equal to the tab</h3>
-                    </div>
+            <div class="d-flex align-items-start">
+                <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                    <button class="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</button>
+                    <button class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</button>
+                    <button class="nav-link" id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</button>
+                    <button class="nav-link" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</button>
+                </div>
+                <div class="tab-content" id="v-pills-tabContent">
+                    <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">...</div>
+                    <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
+                    <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
+                    <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
                 </div>
             </div>
+
 
 
         </div>
