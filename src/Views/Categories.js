3dab6069b43e0262bc7b7961ccf4691ddc87ebb8
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-10-26 14:57:35 +0300

    Update Categories.js

--- src/Views/Categories.js
+++ src/Views/Categories.js
@@ -18,18 +18,18 @@ function Categories() {
                         <div className="laptops-a-container">
                             <div className="row">
                                 <div className="col">
-                                    <div className="card" style={{ width: '18rem' }}>
+                                    <div className="card bg-light" style={{ width: '18rem' }}>
                                         <img className="card-img-top" src={laptop1} alt="Card image cap" />
                                         <div className="card-body">
                                             <h5 className="card-title">Card title</h5>
                                             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                         </div>
                                         <ul className="list-group list-group-flush">
-                                            <li className="list-group-item">Cras justo odio</li>
-                                            <li className="list-group-item">Dapibus ac facilisis in</li>
-                                            <li className="list-group-item">Vestibulum at eros</li>
+                                            <li className="list-group-item bg-light">Cras justo odio</li>
+                                            <li className="list-group-item bg-light">Dapibus ac facilisis in</li>
+                                            <li className="list-group-item bg-light">Vestibulum at eros</li>
                                         </ul>
-                                        <div className="card-body">
+                                        <div className="card-body bg-light">
                                             <a href="#" className="card-link">Card link</a>
                                             <a href="#" className="card-link">Another link</a>
                                         </div>
