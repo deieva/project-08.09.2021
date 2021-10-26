86faef3f5c528fcfcaa618ffa26aff9d787f9ae5
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-10-26 12:02:49 +0300

    update categories

--- src/Views/Categories.js
+++ src/Views/Categories.js
@@ -1,10 +1,11 @@
 import '../Assets/Css/Categories.css';
+import laptop1 from '../Assets/Images/laptop1.jpg';
 
 function Categories() {
 
     return (
 
-        <div>
+        <div className="categories-container">
 
             <div className="d-flex align-items-start">
                 <div className="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
@@ -15,11 +16,26 @@ function Categories() {
                 </div>
                 <div className="tab-content" id="v-pills-tabContent">
                     <div className="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
-                        
+
                         <div className="laptops-a-container">
                             <div className="row">
                                 <div className="col-3">
-                                    
+                                    <div class="card" style={{width: '18rem'}}>
+                                        <img class="card-img-top" src={laptop1} alt="Card image cap"/>
+                                        <div class ="card-body">
+                                        <h5 class ="card-title">Card title</h5>
+                                        <p class ="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        </div>
+                                        <ul class ="list-group list-group-flush">
+                                        <li class ="list-group-item">Cras justo odio</li>
+                                        <li class ="list-group-item">Dapibus ac facilisis in</li>
+                                        <li class ="list-group-item">Vestibulum at eros</li>
+                                        </ul>
+                                        <div class ="card-body">
+                                        <a href="#" class ="card-link">Card link</a>
+                                        <a href="#" class ="card-link">Another link</a>
+                                        </div>
+                                    </div>
                                 </div>
 
 
@@ -31,7 +47,7 @@ function Categories() {
 
                         </div>
 
-                        </div>
+                    </div>
                     <div className="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
                     <div className="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
                     <div className="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
