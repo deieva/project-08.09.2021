b438e39d9bc2f43501605566abdf3c6f0d0b1369
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-10-28 13:26:16 +0300

    Update Categories.js

--- src/Views/Categories.js
+++ src/Views/Categories.js
@@ -256,7 +256,7 @@ function Categories() {
                             <div className="row">
                                 <div className="col">
                                     <div className="card" style={{ width: '18rem' }}>
-                                        <img className="card-img-top" src={laptop1} alt="Card image cap" />
+                                        <img className="card-img-top" src={laptop2} alt="Card image cap" />
                                         <div className="card-body">
                                             <h5 className="card-title">Card title</h5>
                                             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -275,7 +275,7 @@ function Categories() {
 
                                 <div className="col">
                                     <div className="card" style={{ width: '18rem' }}>
-                                        <img className="card-img-top" src={laptop1} alt="Card image cap" />
+                                        <img className="card-img-top" src={laptop2} alt="Card image cap" />
                                         <div className="card-body">
                                             <h5 className="card-title">Card title</h5>
                                             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -311,7 +311,7 @@ function Categories() {
                                 </div>
                                 <div className="col">
                                     <div className="card" style={{ width: '18rem' }}>
-                                        <img className="card-img-top" src={laptop1} alt="Card image cap" />
+                                        <img className="card-img-top" src={laptop2} alt="Card image cap" />
                                         <div className="card-body">
                                             <h5 className="card-title">Card title</h5>
                                             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
